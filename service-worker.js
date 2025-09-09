f95a20ec496ab7efc26a8db1f5cb375b268e08d2
franciscolir <75907947+franciscolir@users.noreply.github.com>
2025-09-09 08:49:27 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,15 @@
-
-const CACHE_NAME = 'placa-cache-v1';
-const urlsToCache = [
-  './',
-  'index.html',
-  'manifest.json',
-  'https://unpkg.com/tesseract.js@5.0.1/dist/tesseract.min.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
-
+const CACHE='alpr-pwa-adv-v1';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','https://unpkg.com/tesseract.js@5/dist/tesseract.min.js','https://docs.opencv.org/4.x/opencv.js'];
+self.addEventListener('install',e=>
+  {e.waitUntil(caches.open(CACHE).then(c=>
+    c.addAll(ASSETS)).then(self.skipWaiting()));
+  });
+self.addEventListener('activate',e=>
+  {e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>
+    caches.delete(k)))));
+   self.clients.claim();
+  });
+self.addEventListener('fetch',e=>
+  {e.respondWith(caches.match(e.request).then(r=>
+    r||fetch(e.request)));
+  });
